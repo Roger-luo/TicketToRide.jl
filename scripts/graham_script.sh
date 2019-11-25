6431c6002586605a1e87a95ef275c87ba25fd6d4
Matt <mbeachphysics@gmail.com>
2019-11-25 16:49:24 -0500

    scripts for graham, and added saving files

---
+++ scripts/graham_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account=rrg-rgmelko-ab
+#SBATCH --ntasks=1
+#SBATCH --mem=10GB
+#SBATCH --time=3:00:00
+#SBATCH --job-name=minimal_VQE_cirq
+#SBATCH --array=1-90
+#SBATCH --output=tfim_data.log
+
+module load nixpkgs/16.09 gcc/7.3.0 julia
+
+julia --project -O3 --check-bounds=no run_graham.jl $SLURM_ARRAY_TASK_ID
