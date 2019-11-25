d463cbf18a97017b1a8bb9f7871222678f6685f5
Roger-luo <hiroger@qq.com>
2019-11-25 15:04:20 -0500

    fix a bug

--- src/fake.jl
+++ src/fake.jl
@@ -1,9 +1,12 @@
 export NoGate
 
 using YaoBase, YaoArrayRegister, YaoBlocks
+using LuxurySparse
 
 struct NoGate <: PrimitiveBlock{1} end
 
 YaoBlocks.apply!(r::AbstractRegister, x::NoGate) = r
 YaoBase.instruct!(r::ArrayReg, x::NoGate, locs) = r
 YaoBlocks.mat_matchreg(r::ArrayReg, x::NoGate) = x
+
+YaoBlocks.mat(::Type{T}, x::NoGate) where T = IMatrix{2, T}()
--- test/circuit.jl
+++ test/circuit.jl
@@ -4,7 +4,7 @@ using Flux.Optimise
 using TicketToRide: train!
 
 function prune(x::RotationGate; threshold=1e-1)
-    if x.theta < threshold
+    if abs(x.theta) < threshold
         return NoGate()
     else
         return x
@@ -22,13 +22,8 @@ train!(opt, 1000, heisenberg1D(4), circuit)
 
 length(filter(x->abs(x)<0.1, parameters(circuit)))/nparameters(circuit)
 
-t = Depolar(0.1, 0.2, 0.3, 0.8)
-rand_state(10) |> repeat(10, Depolar(0.1, 0.2, 0.3, 0.8))
+filter(x->abs(x)<0.1, parameters(circuit))
+nparameters(circuit)
+circuit = prune(circuit);
 
-
-using LinearAlgebra
-
-
-M = mat(heisenberg1D(4))/4
-
-eigmin(Matrix(M))/4
+train!(opt, 1000, heisenberg1D(4), circuit)
